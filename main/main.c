a791a4c1ac5c4a3dbf0946cfc43cca9e262460b0
Sean Andre C. Ancheta <seanandreancheta64@gmail.com>
2025-06-05 12:56:18 +0800

    Implemented LED Code

--- main/main.c
+++ main/main.c
@@ -10,7 +10,11 @@
     1004:   CANNOT SET MPU6050 ACCELEROMETER SCALE FACTOR THROUGH INTERNAL REGISTERS
     1005:   CANNOT INITIALIZE DAC FREQUENCY CHECKER MODULE
     1006:   CANNOT SET MPU6050 LOW PASS FILTER
+    1007:   CANNOT SET DEBUGGING LEDS
+    1008:   CANNOT SET DEBUGGING LEDS QUEUE HANDLE
+    1009:   CANNOT BLINK DEBUG LEDS
     2000:   CANNOT CREATE MAIN LOOP TASK
+    2001:   CANNOT CREATE DEBUG LED LOOP TASK
 */
 
 #include <stdio.h>
@@ -23,24 +27,27 @@
 #include <esp_timer.h>
 #include <freertos/task.h>
 #include <math.h>
+#include "driver/gpio.h"
 
 /*
     Type definitions
 */
 
-typedef struct {
+typedef struct
+{
     float kalmanAngle;
     float kalmanError;
 } kalman_struct_1d_t;
 
-typedef struct {
+typedef struct
+{
     float output;
     float error;
     float prevError;
 } pid_output_t;
 
-
-typedef struct {
+typedef struct
+{
     int8_t sbRoll;
     int8_t sbPitch;
     int8_t sbYaw;
@@ -48,7 +55,18 @@ typedef struct {
     int8_t sbMode;
 } comm_protocol_structure_t;
 
+typedef enum
+{
+    DEBUG_LED_FRONT,
+    DEBUG_LED_BACK,
+} debug_led_t;
 
+typedef struct
+{
+    debug_led_t LED;
+    uint32_t count;
+    uint32_t delayMS;
+} debug_led_command_t;
 
 // DEFINE CONSTANTS HERE THAT WILL BE USED THROUGHOUT THE PROGRAM LOGIC CODE
 
@@ -107,7 +125,24 @@ static const i2c_device_config_t mpu6050_config = {
 };
 static i2c_master_dev_handle_t mpu6050_handle;
 
-
+// LED CODE
+static const unsigned long LED_FRONT_BASE = (1UL << 15);
+static const unsigned long LED_BACK_BASE = (1UL << 5);
+static const gpio_config_t gpio_led_front_config = {
+    .intr_type = GPIO_INTR_DISABLE,
+    .mode = GPIO_MODE_OUTPUT,
+    .pin_bit_mask = LED_FRONT_BASE,
+    .pull_down_en = 0,
+    .pull_up_en = 0,
+};
+static const gpio_config_t gpio_led_back_config = {
+    .intr_type = GPIO_INTR_DISABLE,
+    .mode = GPIO_MODE_OUTPUT,
+    .pin_bit_mask = LED_BACK_BASE,
+    .pull_down_en = 0,
+    .pull_up_en = 0,
+};
+QueueHandle_t debug_led_queue_handle;
 
 // OFFSETS for sensor readings
 
@@ -144,8 +179,7 @@ static float fKalmanAnglePitch = 0.0f;             // Initial guess is 0 because
 static float fKalmanPitchAngleUncertainty = 2 * 2; // Uncertainty of initial angle (since there are no real leveled surface) is 2 degrees
 static kalman_struct_1d_t xKalmanOutput = {
     0.0f,
-    0.0f
-};
+    0.0f};
 
 static const float fKalmanRotationRateVariance = 4 * 4;  // Real life scenario error of 4 degrees / seconds; variance of rotation rate at the current iteration.
 static const float fKalmanAccelerometerVariance = 3 * 3; // Real life scenario error of 3 degrees; variance of accelerometer at the current iteration.
@@ -153,28 +187,19 @@ static const float fKalmanAccelerometerVariance = 3 * 3; // Real life scenario e
 // UART DEBUGGING constants and structures
 static uart_port_t uart_debugging_port = UART_NUM_0; // directs to standard output
 
+// Electronic Speed Controller (ESC) related constants
+static const int32_t ESC_LOW_VALUE = 65536;                 // 2^18 * 0.25; 0.25 is the pulse width ratio with a PWM frequency of 250Hz.
+static const int32_t ESC_HIGH_VALUE = 131072;               // 2^18 * 0.50; 0.50 is the pulse width ratio with a PWM frequency of 250Hz.
+static const int32_t ESC_HIGH_THROTTLE_SAFE_VALUE = 104858; // ESC_HIGH_VALUE * 0.80, only 80% power of motor will be used to prevent over saturation.
+static const int32_t ESC_ANGULAR_SAFE_VALUE = 131000;       // Rounded off to 131000, this is as a safe value for angular motions like roll, pitch, and yaw.
 
-
-//Electronic Speed Controller (ESC) related constants
-static const int32_t ESC_LOW_VALUE =    65536;                  // 2^18 * 0.25; 0.25 is the pulse width ratio with a PWM frequency of 250Hz.
-static const int32_t ESC_HIGH_VALUE =   131072;                 // 2^18 * 0.50; 0.50 is the pulse width ratio with a PWM frequency of 250Hz.
-static const int32_t ESC_HIGH_THROTTLE_SAFE_VALUE = 104858;     // ESC_HIGH_VALUE * 0.80, only 80% power of motor will be used to prevent over saturation.
-static const int32_t ESC_ANGULAR_SAFE_VALUE = 131000;           // Rounded off to 131000, this is as a safe value for angular motions like roll, pitch, and yaw.
-
-//PID related constants
+// PID related constants
 static comm_protocol_structure_t receivedData;
 float fDesiredRollAngle, fDesiredPitchAngle, fDesiredYawAngle;
-float fErrorRollAngle,   fErrorPitchAngle,   fErrorYawAngle;
+float fErrorRollAngle, fErrorPitchAngle, fErrorYawAngle;
 float fPIDOutput;
 
-
-
-
-
-
 // FUNCTIONS AND TASKS
-
-
 
 /// @brief The kalman calculation algorithm for the angles combining gyro and accelerometer calculations.
 /// @param fpKalmanOutput       [out] The array (should be of length 2) that will contain the kalman algorithm calculation. First element = Kalman Angle, Second element = Kalman Angle Uncertainty.
@@ -190,7 +215,7 @@ void IRAM_ATTR kalman_1d_mpu6050(kalman_struct_1d_t *pKalmanOutput, float fKalma
 
     fKalmanUncertainty = fKalmanUncertainty + (SRR_CYCLE_WIDTH * SRR_CYCLE_WIDTH * fKalmanRotationRateVariance);
 
-    float fKalmanGain = fKalmanUncertainty * 1/(1*fKalmanUncertainty + fKalmanAccelerometerVariance);
+    float fKalmanGain = fKalmanUncertainty * 1 / (1 * fKalmanUncertainty + fKalmanAccelerometerVariance);
 
     fKalmanState = fKalmanState + fKalmanGain * (fKalmanMeasurement - fKalmanState);
 
@@ -203,10 +228,8 @@ void IRAM_ATTR kalman_1d_mpu6050(kalman_struct_1d_t *pKalmanOutput, float fKalma
 void IRAM_ATTR PID_controller_function(float fError, float fP, float fI, float fD, float fPrevError, float fPrevIterm)
 {
     float fPTerm = fP * fError;
-    float fIterm = fPrevIterm + fI*(fError+fPrevError)*SRR_CYCLE_WIDTH_SECONDS/2;
-    if (fIterm > ESC_HI)
-
-
+    float fIterm = fPrevIterm + fI * (fError + fPrevError) * SRR_CYCLE_WIDTH_SECONDS / 2;
+    // if (fIterm > ESC_HI)
 }
 
 void IRAM_ATTR debug_print(const char *string, size_t len)
@@ -214,6 +237,54 @@ void IRAM_ATTR debug_print(const char *string, size_t len)
     if (uart_write_bytes(uart_debugging_port, (const void *)string, len) < 0)
     {
         printf("error outputting values to uart port, probably 0, see source code\n");
+    }
+}
+
+void IRAM_ATTR debug_led_front_blink(int count, int delayMS)
+{
+    for (int i = 0; i < count; i++)
+    {
+        gpio_set_level(LED_FRONT_BASE, 1);
+        vTaskDelay(pdMS_TO_TICKS(delayMS));
+        gpio_set_level(LED_FRONT_BASE, 0);
+        vTaskDelay(pdMS_TO_TICKS(delayMS));
+    }
+}
+
+void IRAM_ATTR debug_led_back_blink(int count, int delayMS)
+{
+    for (int i = 0; i < count; i++)
+    {
+        gpio_set_level(LED_BACK_BASE, 1);
+        vTaskDelay(pdMS_TO_TICKS(delayMS));
+        gpio_set_level(LED_BACK_BASE, 0);
+        vTaskDelay(pdMS_TO_TICKS(delayMS));
+    }
+}
+
+void IRAM_ATTR led_gpio_task_loop(void *pvParameters)
+{
+    BaseType_t status;
+    debug_led_command_t received_command;
+    for (;;)
+    {
+        status = xQueueReceive(debug_led_queue_handle, &received_command, portMAX_DELAY);
+        if (status == pdPASS)
+        {
+            if (received_command.LED == DEBUG_LED_FRONT)
+            {
+                debug_led_front_blink(received_command.count, received_command.delayMS);
+            }
+            else
+            {
+                debug_led_back_blink(received_command.count, received_command.delayMS);
+            }
+        }
+        else
+        {
+            char *msg = "1009\n\0";
+            debug_print(msg, 6);
+        }
     }
 }
 
@@ -225,7 +296,7 @@ void IRAM_ATTR flight_controller_loop(void *pvParameters)
     */
     for (;;)
     {
-        // Set frequency of GPIO 25 to 3.3v, use this to debug SRR of quadcopter    
+        // Set frequency of GPIO 25 to 3.3v, use this to debug SRR of quadcopter
 
         // set 4MS delay to match 250Hz
         DBG_DAC_CYCLE_TIME = esp_timer_get_time() + SRR_CYCLE_WIDTH_MICRO;
@@ -316,10 +387,34 @@ void app_main(void)
     // print boot message
     debug_print(BOOT_MSG, BOOT_MSG_LEN);
 
+    // setup debug led queue handle
+    debug_led_queue_handle = xQueueCreate(5, sizeof(debug_led_command_t));
+
+    if (debug_led_queue_handle == NULL)
+    {
+        // set debugging LEDS
+        char *msg = "1008\n\0";
+        debug_print(msg, 6);
+    }
+
+    // setup gpio for setting base current for the LEDS
+    if (gpio_config(&gpio_led_front_config) != ESP_OK)
+    {
+        // set debugging LEDS
+        char *msg = "1007\n\0";
+        debug_print(msg, 6);
+    }
+
+    if (gpio_config(&gpio_led_back_config) != ESP_OK)
+    {
+        // set debugging LEDS
+        char *msg = "1007\n\0";
+        debug_print(msg, 6);
+    }
+
     // Initialize sensors
     if (i2c_new_master_bus(&mcu_master_device_config, &mcu_master_device_handle) != ESP_OK)
     {
-        // set debugging LEDS
         char *msg = "1000\n\0";
         debug_print(msg, 6);
     }
@@ -363,6 +458,12 @@ void app_main(void)
     if (xTaskCreatePinnedToCore(flight_controller_loop, "FLIGHT_CONTROLLER", 10000, NULL, 1, NULL, 1) != pdPASS)
     {
         char *msg = "2000\n\0";
+        debug_print(msg, 6);
+    }
+
+    if (xTaskCreatePinnedToCore(led_gpio_task_loop, "DEBUG_LEDS_PROCESS", 10000, NULL, 1, NULL, 0) != pdPASS)
+    {
+        char *msg = "2001\n\0";
         debug_print(msg, 6);
     }
 }
